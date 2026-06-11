1f91d41457ba70833f9e12eacfc46f50a3f6ed08
Joana Inácio <inaciojoana@hotmail.com>
2026-06-11 12:54:56 +0100

    Add GitHub link to footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ export default function App() {
     <div className="App">
       <h1 className="app-title">Weather App</h1>
       <Weather defaultCity="Lisbon" />
+
+      <footer className="app-footer">
+        
+          href="https://github.com/JoanaInacio/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code on GitHub
+        </a>
+        , coded by Joana Inácio
+      </footer>
     </div>
   );
 }
